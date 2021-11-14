e5d65d80d79525b17d34293557add239e3647eb5
BTV <horsfalm@gmail.com>
2021-11-14 13:45:24 +1100

    enable multiple battles

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,7 +18,10 @@ var enemyAttack = 12;
 
 var fight = function(enemyName) {
     // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    // window.alert("Welcome to Robot Gladiators!");
+
+    // Repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
 
     // Ask the player if they would like to fight or skip battle
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -76,10 +79,14 @@ var fight = function(enemyName) {
     fight()
 }
 
+}
+
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
 
 // fight();
